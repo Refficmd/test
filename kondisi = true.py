1631b6b28a1f6fde58bd3f4dcaeff9543a021021
Refficmd <reffi.fadillah01@gmail.com>
2024-09-24 15:54:00 +0800

    Add files via upload
    testing

---
+++ kondisi = true.py
@@ -0,0 +1,16 @@
+umur = float(input ("masukkan umur= "))   
+
+if umur >=46:
+    print ("lansia")
+
+elif umur >=26:
+    print ("dewasa")
+
+elif umur >=12:
+    print("remaja")
+
+elif umur >=5:
+    print("kanak-kanak")
+
+else:
+    print("balita")
